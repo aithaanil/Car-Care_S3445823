955cfc03b477ca55c37fc7e060e5aee9e856ec52
aithaanil <aithaanil0612@gmail.com>
2025-04-28 21:25:24 +0100

    Car-Care changes

--- app/src/main/java/uk/ac/tees/mad/carcare/model/dataclass/firebase/SignInResult.kt
+++
@@ -1,9 +0,0 @@
-package uk.ac.tees.mad.carcare.model.dataclass.firebase
-
-data class SignInResult(
-    val data: UserDataGoogle?, val errorMessage: String?
-)
-
-data class UserDataGoogle(
-    val userId: String, val username: String?, val profilePictureUrl: String?
-)
--- app/src/main/java/uk/ac/tees/mad/carcare/model/dataclass/firebase/SignInState.kt
+++
@@ -1,5 +0,0 @@
-package uk.ac.tees.mad.carcare.model.dataclass.firebase
-
-data class SignInState(
-    val isSignInSuccessful: Boolean = false, val signInError: String? = null
-)
